e79967dcd1009f2e90a34fb019f6c72461ee7b00
Camila Brognara <cbrognara@gmail.com>
2022-02-10 21:22:25 -0300

    consertado bug home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -28,6 +28,7 @@ const Home = (): JSX.Element => {
   const cartItemsAmount = cart.reduce((sumAmount, product) => {
     const newSumAmount = { ...sumAmount }
     newSumAmount[product.id] = product.amount
+    return newSumAmount
   }, {} as CartItemsAmount)
 
   useEffect(() => {
